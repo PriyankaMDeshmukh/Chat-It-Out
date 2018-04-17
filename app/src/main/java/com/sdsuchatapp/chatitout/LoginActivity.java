73c724f9a9baf799779efa8897e7743c142d50ec
Akash Banerjee <akashbnrj07@gmail.com>
2018-04-16 20:40:32 -0700

    firebase auth phone number added verification

--- app/src/main/java/com/sdsuchatapp/chatitout/LoginActivity.java
+++ app/src/main/java/com/sdsuchatapp/chatitout/LoginActivity.java
@@ -2,12 +2,103 @@ package com.sdsuchatapp.chatitout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final String KEY_VERIFY_IN_PROGRESS = "key_verify_in_progress";
+    private EditText phoneNumber;
+    private FirebaseAuth auth;
+    private boolean verificationInProgress = false;
+    private PhoneAuthProvider.ForceResendingToken mResendToken;
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks callbacks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Restore instance state
+        if (savedInstanceState != null) {
+            onRestoreInstanceState(savedInstanceState);
+        }
+
         setContentView(R.layout.activity_login);
+        phoneNumber = findViewById(R.id.phoneNumber);
+        auth = FirebaseAuth.getInstance();
+
+
     }
+
+    // [START on_start_check_user]
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = auth.getCurrentUser();
+        updateUI(currentUser);
+
+        // [START_EXCLUDE]
+        if (verificationInProgress && validatePhoneNumber()) {
+            startPhoneNumberAuthentication(phoneNumber.getText().toString());
+        }
+        // [END_EXCLUDE]
+    }
+    // [END on_start_check_user]
+
+    private void updateUI(FirebaseUser currentUser) {
+
+    }
+
+
+
+    private boolean validatePhoneNumber() {
+        String phone = phoneNumber.getText().toString();
+        if (TextUtils.isEmpty(phone)) {
+            phoneNumber.setError("Invalid phone number.");
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(KEY_VERIFY_IN_PROGRESS, verificationInProgress);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        verificationInProgress = savedInstanceState.getBoolean(KEY_VERIFY_IN_PROGRESS);
+    }
+
+    public void authenticate(View view){
+        startPhoneNumberAuthentication(view.findViewById(R.id.phoneNumber).toString());
+    }
+
+    private void startPhoneNumberAuthentication(String phoneNumber) {
+        // [START start_phone_auth]
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                phoneNumber,        // Phone number to verify
+                60,                 // Timeout duration
+                TimeUnit.SECONDS,   // Unit of timeout
+                this,               // Activity (for callback binding)
+                callbacks);        // OnVerificationStateChangedCallbacks
+        // [END start_phone_auth]
+
+        verificationInProgress = true;
+    }
+
+
+
+
 }
